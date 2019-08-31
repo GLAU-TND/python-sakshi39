c3efdbcbc4f08cd684199d6a4566bfef842c9036
sakshi39 <srivastavsakshi2611@gmail.com>
2019-08-31 22:58:31 +0530

    final commit

--- bottelnecksolution.py
+++ bottelnecksolution.py
@@ -1,10 +1,8 @@
 n = int(input())
 rb = list(map(int, input().rstrip().split()))
-for i in range(8):
-    for j in range(8):
-        if( rb[i] < rb[j] ):
-            rb.pop(i)
-p=len(rb)
-for i in range(p):
-    print(rb[i])
-
+c=[]
+for i in range(n):
+    d = rb.count(rb[i])
+    c.append(d)
+g = max(c)
+print(g8)
